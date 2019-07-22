83c24f913fa80be418ed0a2e6438a2d1dd573f47
Chris Weldon <Chris.Weldon@wolterskluwer.com>
2019-07-22 14:17:54 -0500

    Now presenting the current iteration's items in a table on the Overview page.

--- src/Samples/Hub/OverviewTab.tsx
+++ src/Samples/Hub/OverviewTab.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as SDK from "azure-devops-extension-sdk";
-import { CommonServiceIds, IProjectPageService } from "azure-devops-extension-api";
+import { CommonServiceIds, getClient, IProjectPageService } from "azure-devops-extension-api";
+import { WorkItemTrackingRestClient, Wiql, WorkItemReference } from "azure-devops-extension-api/WorkItemTracking";
 
 import { Dropdown } from "azure-devops-ui/Dropdown";
 import { ListSelection } from "azure-devops-ui/List";
@@ -9,10 +10,13 @@ import { IListBoxItem } from "azure-devops-ui/ListBox";
 export interface IOverviewTabState {
     userName?: string;
     projectName?: string;
+    iterationPath?: string;
+    areaPath?: string;
     iframeUrl?: string;
     extensionData?: string;
     extensionContext?: SDK.IExtensionContext;
     selection: ListSelection;
+    workItems: WorkItemReference[];
 }
 
 export class OverviewTab extends React.Component<{}, IOverviewTabState> {
@@ -25,6 +29,9 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
 
         this.state = {
             iframeUrl: window.location.href,
+            iterationPath: "Azure DevOps Sprint Review\\Iteration 1",
+            areaPath: "Azure DevOps Sprint Review\\Core",
+            workItems: [],
             selection
         };
     }
@@ -35,7 +42,7 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
 
     private async initializeState(): Promise<void> {
         await SDK.ready();
-        
+
         const userName = SDK.getUser().displayName;
         this.setState({
             userName,
@@ -44,8 +51,20 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
 
         const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
         const project = await projectService.getProject();
+
         if (project) {
             this.setState({ projectName: project.name });
+            console.debug(`project name set to: ${project.name}`);
+
+            const client = getClient(WorkItemTrackingRestClient);
+            let wiqlString : Wiql = { query: `SELECT [System.Id] FROM workitems WHERE [System.TeamProject] = '${project.name}' AND [System.AreaPath] = '${this.state.areaPath}' AND [System.WorkItemType] = 'User Story' AND [System.IterationPath] = '${this.state.iterationPath}'` };
+            console.debug(`WIQL Query: ${wiqlString.query}`);
+            const results = await client.queryByWiql(wiqlString, project.name);
+            this.setState({ workItems: results.workItems });
+            console.debug(results);
+            results.workItems.forEach(element => {
+                console.log("Work Item " + element.id + " in iteration " + this.state.iterationPath);
+            });
         }
     }
 
@@ -79,6 +98,18 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
                         <div>Extension version: {extensionContext.version}</div>
                     </>
                 }
+                <div className="work-item-table">
+                    <table>
+                        <tbody>
+                        {this.state.workItems.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         );
     }
